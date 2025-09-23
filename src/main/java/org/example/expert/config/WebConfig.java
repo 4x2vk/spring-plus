b8aa332cf9aad1e5baa70a4496ea04ddf5e1b039
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-09-23 11:33:19 +0900

    [level 3-11] remove unnecessary imports from SecurityConfig and WebConfig

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,13 +1,7 @@
 package org.example.expert.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 }
